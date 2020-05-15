d199c0a5b081d895b7c2dd5cf1fc0389f26d6a6a
Gennady G <rockietm@gmail.com>
2020-05-15 17:58:25 +0400

    replaced icons with base64

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -19,21 +19,18 @@ export default class HelloWorld extends React.Component < IHelloWorldProps, {} >
 
     console.log("Show notifications..");
 
-    toastr.info("Please select user", "No user selected");
-    toastr.success("Please select user", "No user selected");
-    toastr.warning("Please select user", "No user selected");
-    toastr.error("Please select user", "No user selected");
+    toastr.info("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "Header message");
+    toastr.success("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "Header message");
+    toastr.warning("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "Header message");
+    toastr.error("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "Header message");
 
   }
 
   protected init(): void {
 
-    console.log("Initialize..");
-
       // Load the Toastr CSS
       SPComponentLoader.loadCss('https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css');
 
-      console.log("Third-party js/css loaded.");
 
       //***********************
       //Toastr Options
@@ -43,7 +40,9 @@ export default class HelloWorld extends React.Component < IHelloWorldProps, {} >
       // styles.topRight and styles.topLeft take into account the SuiteBar
       // there are also the native toast-bottom-right and toast-bottom-left
       toastr.options.positionClass = `${styles.topRight} ${styles.helloWorld}`;
-      toastr.options.preventDuplicates = false;
+
+      toastr.options.preventDuplicates = false;   // Prevent duplicates to FALSE
+
       toastr.options.newestOnTop = false; //Ensures the first toast we send is on top
       toastr.options.timeOut = 0; //Prevents auto dismissal
       toastr.options.extendedTimeOut = 0; //Prevents auto dismissal during hover
@@ -57,23 +56,35 @@ export default class HelloWorld extends React.Component < IHelloWorldProps, {} >
       //We are unable to use the ms-bgColor styles since the Toast CSS loads
       // later and takes precedence, so we use our own color classes
       // For more background on this issue, see this article: https://dev.office.com/sharepoint/docs/spfx/web-parts/guidance/office-ui-fabric-integration
-      toastr.options.titleClass = 'ms-font-m ms-fontWeight-semibold';
-      toastr.options.messageClass = 'ms-font-s';
-      toastr.options.iconClasses = {
-        info: `${styles.info} ${styles.fabricIcon} ms-Icon--Info`,
-        warning: `${styles.warning} ${styles.fabricIcon} ms-Icon--Warning`,
-        error: `${styles.error} ${styles.fabricIcon} ms-Icon--Error`,
-        success: `${styles.success} ${styles.fabricIcon} ms-Icon--Completed`
-      };
 
-    console.log("Initialized.");
+
+      /* Replaced UI Fabric with base64 converted icons: */
+
+      // toastr.options.titleClass = 'ms-font-m ms-fontWeight-semibold';
+      // toastr.options.messageClass = 'ms-font-s';
+      // toastr.options.iconClasses = {
+      //   info: `${styles.info} ${styles.fabricIcon} ms-Icon--Info`,
+      //   warning: `${styles.warning} ${styles.fabricIcon} ms-Icon--Warning`,
+      //   error: `${styles.error} ${styles.fabricIcon} ms-Icon--Error`,
+      //   success: `${styles.success} ${styles.fabricIcon} ms-Icon--Completed`
+      // };
+
+      toastr.options.titleClass = 'my-toast-title';
+      toastr.options.messageClass = 'my-toast-message';
+
+      toastr.options.iconClasses = {
+        info: `${styles.info} my-toast-info`,
+        warning: `${styles.warning} my-toast-warning`,
+        error: `${styles.error} my-toast-error`,
+        success: `${styles.success} my-toast-success`
+      };
 
   }
 
   public render(): React.ReactElement<IHelloWorldProps> {
 
     // init toastr
-    this.init();  
+    this.init();
 
     return(
       <div className = { styles.helloWorld } >
